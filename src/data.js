5609750de03b00752dcfcd7c5086da1abf6c9aa1
jvariara <jvariara@gmail.com>
2023-07-10 21:47:12 -0400

    Added reddit clone project

--- src/data.js
+++ src/data.js
@@ -15,21 +15,29 @@ import MovieLanding from "./assets/projects/MovieSearcher/MovieLanding.png";
 import MovieList from "./assets/projects/MovieSearcher/MovieList.png";
 import MovieDetail from "./assets/projects/MovieSearcher/MovieDetail.png";
 
-import NetflixLanding from "./assets/projects/Netflix/NetflixLanding.png"
-import NetflixSignIn from "./assets/projects/Netflix/NetflixSignIn.png"
+import NetflixLanding from "./assets/projects/Netflix/NetflixLanding.png";
+import NetflixSignIn from "./assets/projects/Netflix/NetflixSignIn.png";
 
-import PromptLanding from "./assets/projects/Promptopia/Landing.png"
-import PromptProfile from "./assets/projects/Promptopia/Profile.png"
-import PromptEdit from "./assets/projects/Promptopia/Edit.png"
-import PromptCreate from "./assets/projects/Promptopia/Create.png"
+import PromptLanding from "./assets/projects/Promptopia/Landing.png";
+import PromptProfile from "./assets/projects/Promptopia/Profile.png";
+import PromptEdit from "./assets/projects/Promptopia/Edit.png";
+import PromptCreate from "./assets/projects/Promptopia/Create.png";
 
-import CircleUBLanding from "./assets/projects/CircleUB/Landing.png"
-import CircleUBLogin from "./assets/projects/CircleUB/Login.png"
-import CircleUBUserHomepage from "./assets/projects/CircleUB/UserHomepage.png"
-import CircleUBProfile from "./assets/projects/CircleUB/Profile.png"
-import CircleUBOtherProfile from "./assets/projects/CircleUB/OtherProfile.png"
-import CircleUBCommentPage from "./assets/projects/CircleUB/CommentPage.png"
-import CircleUBDM from "./assets/projects/CircleUB/DM.png"
+import CircleUBLanding from "./assets/projects/CircleUB/Landing.png";
+import CircleUBLogin from "./assets/projects/CircleUB/Login.png";
+import CircleUBUserHomepage from "./assets/projects/CircleUB/UserHomepage.png";
+import CircleUBProfile from "./assets/projects/CircleUB/Profile.png";
+import CircleUBOtherProfile from "./assets/projects/CircleUB/OtherProfile.png";
+import CircleUBCommentPage from "./assets/projects/CircleUB/CommentPage.png";
+import CircleUBDM from "./assets/projects/CircleUB/DM.png";
+
+import BreaditCommunity from "./assets/projects/Reddit/BreaditCommunity.png";
+import BreaditFeed from "./assets/projects/Reddit/BreaditFeed.png";
+import BreaditSignIn from "./assets/projects/Reddit/BreaditSignIn.png";
+import BreaditCreateCommunity from "./assets/projects/Reddit/BreaditCreateCommunity.png";
+import BreaditCreatePost from "./assets/projects/Reddit/BreaditCreatePost.png";
+import BreaditPost from "./assets/projects/Reddit/BreaditPost.png";
+import BreaditSettings from "./assets/projects/Reddit/BreaditSettings.png";
 
 import ReactImg from "./assets/languages/react.png";
 import JSImg from "./assets/languages/javascript.png";
@@ -39,6 +47,7 @@ import PythonImg from "./assets/languages/python.png";
 import JavaImg from "./assets/languages/java.png";
 import ScalaImg from "./assets/languages/scala.png";
 import CImg from "./assets/languages/c.png";
+import NextImg from "./assets/languages/Next.js.png";
 
 export const projects = [
   {
@@ -48,8 +57,15 @@ export const projects = [
     description:
       "Transformed a completely static HTML, CSS, JavaScript and React single page application into an interactive user interface through the use of animations, transitions and carousels.",
     tech: ["React", "JavaScript", "CSS", "HTML"],
-    pictures: [NFTLanding, NFTCollections, NFTExplore, NFTItem, NFTAuthor, NFTLoading],
-    certificate: true
+    pictures: [
+      NFTLanding,
+      NFTCollections,
+      NFTExplore,
+      NFTItem,
+      NFTAuthor,
+      NFTLoading,
+    ],
+    certificate: true,
   },
   {
     id: 2,
@@ -59,27 +75,61 @@ export const projects = [
     description:
       "Created a fully dynamic E-Commerce website with a working shopping cart, item details page with recommendations, and filtering.",
     tech: ["React", "JavaScript", "CSS", "HTML"],
-    pictures: [LibraryLanding, LibraryBooks, LibraryBook, LibraryCart, LibraryLoading],
+    pictures: [
+      LibraryLanding,
+      LibraryBooks,
+      LibraryBook,
+      LibraryCart,
+      LibraryLoading,
+    ],
   },
   {
     id: 3,
+    title: "CircleUB",
+    url: "https://webdev.cse.buffalo.edu/hci/teams/pentagon",
+    description:
+      "Social Media application catered towards University at Buffalo students to find clubs and events. Created with React",
+    tech: ["React", "CSS", "Zenhub", "Figma"],
+    pictures: [
+      CircleUBLanding,
+      CircleUBLogin,
+      CircleUBUserHomepage,
+      CircleUBCommentPage,
+      CircleUBDM,
+      CircleUBProfile,
+      CircleUBOtherProfile,
+    ],
+  },
+  {
+    id: 4,
+    title: "Breadit",
+    url: "https://reddit-clone-rosy.vercel.app/",
+    github: "https://github.com/jvariara/reddit-clone",
+    description:
+      "Application clone based on the Social Media App Reddit. Create/join communities, publish, comment, and rate subreddits.",
+    tech: ["React", "Next.js 13", "Tailwind CSS", "Auth", "Prisma", "MySQL"],
+    pictures: [
+      BreaditFeed,
+      BreaditSignIn,
+      BreaditCommunity,
+      BreaditPost,
+      BreaditCreatePost,
+      BreaditCreateCommunity,
+      BreaditSettings,
+    ],
+  },
+  {
+    id: 5,
     title: "Promptopia",
     url: "https://promptopia-jvariara.vercel.app/",
     github: "https://github.com/jvariara/Promptopia",
-    description: "Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts. Created with Next.js and MongoDB",
+    description:
+      "Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts. Created with Next.js and MongoDB",
     tech: ["Next.js", "MongoDB", "Tailwind CSS"],
     pictures: [PromptLanding, PromptProfile, PromptCreate, PromptEdit],
   },
   {
-    id: 4,
-    title: "CircleUB",
-    url: "https://webdev.cse.buffalo.edu/hci/teams/pentagon",
-    description: "Social Media application catered towards University at Buffalo students to find clubs and events. Created with React",
-    tech: ["React", "CSS", "Zenhub", "Figma"],
-    pictures: [CircleUBLanding, CircleUBLogin, CircleUBUserHomepage, CircleUBCommentPage, CircleUBDM, CircleUBProfile, CircleUBOtherProfile]
-  },
-  {
-    id: 5,
+    id: 6,
     title: "Movie/TV Searcher",
     url: "https://movies-pi-jade.vercel.app/",
     github: "https://github.com/jvariara/Movies",
@@ -89,7 +139,7 @@ export const projects = [
     pictures: [MovieLanding, MovieList, MovieDetail],
   },
   {
-    id: 6,
+    id: 7,
     title: "Netflix Clone",
     url: "https://netflix-clone-1dcd3.web.app/",
     github: "https://github.com/jvariara/netflix-clone",
@@ -104,41 +154,41 @@ export const technologies = [
   {
     id: 1,
     language: "React",
-    picture: ReactImg
+    picture: ReactImg,
   },
   {
     id: 2,
-    language: "JavaScript",
-    picture: JSImg
+    language: "Next.js",
+    picture: NextImg,
   },
   {
     id: 3,
-    language: "HTML5",
-    picture: HTMLImg
+    language: "JavaScript",
+    picture: JSImg,
   },
   {
     id: 4,
-    language: "CSS3",
-    picture: CSSImg
+    language: "HTML5",
+    picture: HTMLImg,
   },
   {
     id: 5,
-    language: "Python",
-    picture: PythonImg
+    language: "CSS3",
+    picture: CSSImg,
   },
   {
     id: 6,
-    language: "Java",
-    picture: JavaImg
+    language: "Python",
+    picture: PythonImg,
   },
   {
     id: 7,
-    language: "C",
-    picture: CImg
+    language: "Java",
+    picture: JavaImg,
   },
   {
     id: 8,
-    language: "Scala",
-    picture: ScalaImg
+    language: "C",
+    picture: CImg,
   },
-]
+];
